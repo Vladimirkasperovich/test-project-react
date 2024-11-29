bdc95c5f09dd95b22ebbb63b5bf0ad7f57b28b21
vovak <vova.kasper.boy@gmail.com>
2024-11-29 16:55:34 +0300

    feat: third commit

--- src/features/store/components/store-card/store-card.tsx
+++ src/features/store/components/store-card/store-card.tsx
@@ -5,12 +5,15 @@ import { FC } from 'react'
 interface Props {
   product: Product
 }
+
 export const StoreCard: FC<Props> = ({ product }) => {
   return (
     <div className={styles.main}>
       <div className={styles.block}>
-        <h5>{product.title}</h5>
-        <img src={product.image} alt={'product image'} />
+        <h5 className={styles.product__title}>{product.title}</h5>
+        <img className={styles.product__image} src={product.image} alt={'product image'} />
+        <p className={styles.product__description}>{product.description}</p>
+        <p className={styles.product__price}>{product.price}</p>
       </div>
     </div>
   )
--- src/pages/store/store-page.tsx
+++ src/pages/store/store-page.tsx
@@ -2,18 +2,15 @@ import { StoreCard, useGetProducts } from '../../features'
 import styles from './store-page.module.scss'
 
 export const StorePage = () => {
-  const { error, loading, products } = useGetProducts()
+  const { loading, products } = useGetProducts()
   if (loading) {
     return <h1>...Loading</h1>
   }
   return (
     <div className={styles.main}>
-      {error && <p>{error}</p>}
-      <div>
-        {products.map((product) => (
-          <StoreCard key={product.id} product={product} />
-        ))}
-      </div>
+      {products.map((product) => (
+        <StoreCard key={product.id} product={product} />
+      ))}
     </div>
   )
 }
